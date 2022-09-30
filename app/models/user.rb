37541eb572227a22927a2c06ac02bb976c778b8b
youhei.iwahashi <y.iwahashi0906@gmail.com>
2022-09-30 12:32:10 +0000

    ユーザー登録

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  validates :age, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  has_secure_password
+end
