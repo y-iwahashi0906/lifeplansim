6ea461d7844a66b487210ad560a72627f3e7597e
y-iwahashi0906 <y.iwahashi0906@gmail.com>
2022-10-13 13:52:10 +0000

    use formatter

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,23 +1,23 @@
 class EventsController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_asset_sim, only: [:edit, :update, :destroy]
-  
+
   def new
     @event = Event.new
   end
 
   def create
-    @asset_sim = current_user.asset_sims.find_by( user_id: current_user.id)
+    @asset_sim = current_user.asset_sims.find_by(user_id: current_user.id)
     unless @asset_sim
       redirect_to root_url
     end
     @event = @asset_sim.events.build(event_params)
 
     if @event.save
-      flash[:success] = '正常に設定されました'
+      flash[:success] = "正常に設定されました"
       redirect_to root_url
     else
-      flash.now[:danger] = 'イベントが設定できませんでした'
+      flash.now[:danger] = "イベントが設定できませんでした"
       render :new
     end
   end
@@ -29,10 +29,10 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
     if @event.update(event_params)
-      flash[:success] = '正常に更新されました'
+      flash[:success] = "正常に更新されました"
       redirect_to root_url
     else
-      flash.now[:danger] = '更新されませんでした'
+      flash.now[:danger] = "更新されませんでした"
       render :edit
     end
   end
@@ -41,16 +41,15 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     @event.destroy
 
-    flash[:success] = '正常に削除されました'
+    flash[:success] = "正常に削除されました"
     redirect_to root_url
   end
-  
-  
+
   # Strong Parameter
   def event_params
     params.require(:event).permit(:name, :value, :age, :term, :description, :isvalid)
   end
-  
+
   def correct_asset_sim
     @asset_sim = current_user.asset_sims.find_by(user_id: current_user.id) #検索方法は見直し
     unless @asset_sim
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,21 +6,20 @@ class SessionsController < ApplicationController
     email = params[:session][:email].downcase
     password = params[:session][:password]
     if login(email, password)
-      flash[:success] = 'ログインに成功しました。'
+      flash[:success] = "ログインに成功しました。"
       redirect_to root_url
     else
-      flash.now[:danger] = 'ログインに失敗しました。'
+      flash.now[:danger] = "ログインに失敗しました。"
       render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = 'ログアウトしました。'
+    flash[:success] = "ログアウトしました。"
     redirect_to root_url
   end
-  
-  
+
   private
 
   def login(email, password)
